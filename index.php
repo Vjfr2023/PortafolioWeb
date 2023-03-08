6e703344fde29f8bd45e797f3f67cf77e59872ea
Smak <110242074+Vjfr2023@users.noreply.github.com>
2023-03-08 15:12:16 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<link rel="icon" href="images/lg.ico">
+		<title>Portafolio Victor Fernandes</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		
+
+		<script type="text/javascript" language="Javascript">
+		document.oncontextmenu = function(){return false}
+		</script>
+
+	</head>
+	
+	<body class="is-preload">
+
+			<div id="wrapper">
+
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+
+						<section id="sidebar">
+							<div class="inner" >
+								<nav>
+									<ul>
+										<li><a href="#intro">Bienvenidos</a></li>
+										<li><a href="#one">Conocimientos</a></li>
+										<li><a href="#two">Empresas</a></li>
+										<li><a href="#three">Proyectos</a></li>
+										<li><a href="#four">Contacto</a></li>
+									</ul>
+								</nav>
+							</div>
+						</section> 
+						<div class="inner">
+							<center>
+							<div class="actions">
+								<a href="#one" class="scrolly btn1">
+									<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-rocket" viewBox="0 0 16 16" style="color: cyan;">
+										<path d="M8 8c.828 0 1.5-.895 1.5-2S8.828 4 8 4s-1.5.895-1.5 2S7.172 8 8 8Z"/>
+										<path d="M11.953 8.81c-.195-3.388-.968-5.507-1.777-6.819C9.707 1.233 9.23.751 8.857.454a3.495 3.495 0 0 0-.463-.315A2.19 2.19 0 0 0 8.25.064.546.546 0 0 0 8 0a.549.549 0 0 0-.266.073 2.312 2.312 0 0 0-.142.08 3.67 3.67 0 0 0-.459.33c-.37.308-.844.803-1.31 1.57-.805 1.322-1.577 3.433-1.774 6.756l-1.497 1.826-.004.005A2.5 2.5 0 0 0 2 12.202V15.5a.5.5 0 0 0 .9.3l1.125-1.5c.166-.222.42-.4.752-.57.214-.108.414-.192.625-.281l.198-.084c.7.428 1.55.635 2.4.635.85 0 1.7-.207 2.4-.635.067.03.132.056.196.083.213.09.413.174.627.282.332.17.586.348.752.57l1.125 1.5a.5.5 0 0 0 .9-.3v-3.298a2.5 2.5 0 0 0-.548-1.562l-1.499-1.83ZM12 10.445v.055c0 .866-.284 1.585-.75 2.14.146.064.292.13.425.199.39.197.8.46 1.1.86L13 14v-1.798a1.5 1.5 0 0 0-.327-.935L12 10.445ZM4.75 12.64C4.284 12.085 4 11.366 4 10.5v-.054l-.673.82a1.5 1.5 0 0 0-.327.936V14l.225-.3c.3-.4.71-.664 1.1-.861.133-.068.279-.135.425-.199ZM8.009 1.073c.063.04.14.094.226.163.284.226.683.621 1.09 1.28C10.137 3.836 11 6.237 11 10.5c0 .858-.374 1.48-.943 1.893C9.517 12.786 8.781 13 8 13c-.781 0-1.517-.214-2.057-.607C5.373 11.979 5 11.358 5 10.5c0-4.182.86-6.586 1.677-7.928.409-.67.81-1.082 1.096-1.32.09-.076.17-.135.236-.18Z"/>
+										<path d="M9.479 14.361c-.48.093-.98.139-1.479.139-.5 0-.999-.046-1.479-.139L7.6 15.8a.5.5 0 0 0 .8 0l1.079-1.439Z"
+										style="
+										width: 300px;
+										height: 300px;
+										padding: 10px;
+										border: 0px solid green; "/>
+									</svg>
+								</a>
+							</div>
+							<span><h1>¡Saludos!</h1></span>
+							<p style="color: rgb(255, 255, 255);">Soy Victor Fernandes, un Desarrollador Web</p>
+							<div class="actions">
+								<svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
+									<path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"
+									style="color: rgb(1, 214, 1);"/>
+								  </svg>
+								</a>
+							</div>
+							</center>
+						</div>
+					</section>
+
+					<style>
+					.carousel-inner img {
+						width: 100%;
+						height: 100%;
+					}
+					</style>
+					<div id="carouselExampleIndicators" class="carousel slide">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+							<img src="certificados/pythonforbenigers.png" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+							<img src="certificados/" class="d-block w-100" alt="...">
+							</div>
+							<div class="carousel-item">
+							<img src="certificados/" class="d-block w-100" alt="...">
+							</div>
+						</div>
+						<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Anterior</span>
+						</button>
+						<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Siguiente</span>
+						</button>
+					</div>
+
+					<br> <br>
+
+						<section id="two" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+							<section>
+								<style>emp:hover{filter: blur(5px);}</style>
+								<emp><a href="https://multimaxstore.com/" ><img src="images/Multimax.jpg" alt="" width="400" style="padding: 1cm;" /></a></emp>
+							</section>
+							<section>
+								<emp> <a href="http://www.unicasa.com.ve/" ><img src="images/unicasa.jpg" alt="" width="400" style="padding: 1cm;" /></a></emp>
+							</section>
+							<section> 
+								<emp> <a href="https://www.masxmenosve.com/" ><img src="images/MasPorMenos.PNG" alt="" width="400" style="padding: 1cm;" /></a></emp>
+							</section>
+							<section>
+								<emp> <a href="https://www.instagram.com/aikoz_ve/" ><img src="images/Aikoz.PNG" alt="" width="400" style="padding: 1cm;" /></a></emp>
+							</section>
+							<section>
+								<emp> <a href="https://www.instagram.com/masshopping/" ><img src="images/masshoping.jpg" alt="" width="400" style="padding: 1cm;" /></a></emp>
+							</section>
+							<section>
+								<emp> <a href="https://www.instagram.com/tiendasdaka/" ><img src="images/Daka.jpg" alt="" width="400" style="padding: 1cm;" /></a></emp>
+							</section>
+						</section>
+
+					<br>
+
+					<div style="position: static;">
+						<marquee behavior="" direction="right">
+							<section class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+								<section>
+										<div class="circular--portrait">
+											<img src="https://uploads.sololearn.com/uploads/courses/1161.png">
+										</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1051.png">
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1075.png">
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1081.png">
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1089.png">
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1093.png">
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1014.png">
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1160.png" >
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1097.png">
+									</div>
+								</section>
+								<section>
+									<div class="circular--portrait" >
+										<img src="https://uploads.sololearn.com/uploads/courses/1147.png">
+									</div>
+								</section>
+							</section>
+						</marquee>
+					</div>
+
+					<br>
+					<div id="three">
+						<div id="contenedor">
+							<div class="contenedor-loader">
+								<div class="loader"></div>
+							</div>
+							<div class="cargando">Muy Pronto...</div>
+						</div>
+					</div>
+
+
+					<section id="" class="wrapper style1 fade-up">
+						<div class="inner">
+							<section id="four" ><h2>Contactame</h2></section>
+							<p>Si necesitas ayuda en algún proyecto, no dudes en ponerte en contacto conmigo.</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Nombre y Apellido</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Correo</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field half">
+												<label for="asunto">Asunto</label>
+												<input type="text" name="asunto" id="asunto" />
+											</div>
+											<div class="field half">
+												<label for="telefono">Telefono</label>
+												<input type="text" placeholder="(+58)" name="telefono" id="telefono" />
+											</div>
+											<div class="field">
+												<label for="message">Descripcion</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Enviar Mensaje</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Email</h3>
+											<p>victorfernandesrodriguez@gmail.com</p>
+										</li>
+										<li>
+											<h3>Telefono</h3>
+											<span>(+58) 412-9610248</span>
+											<span>(+58) 414-8123632</span>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="https://www.facebook.com/profile.php?id=100062037317807" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+												<li><a href="https://github.com/Vjfr2023" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://www.instagram.com/vjfr_2604/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="https://www.linkedin.com/in/victor-fernandes-67921a188/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+												<li><a href="https://www.google.com/intl/es/gmail/about/" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+  												<path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+												</svg> </a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<center><li style="color: rgba(255, 255, 255, 0.637);">&copy; Copyright 2023.</li><li style="color: rgba(255, 255, 255, 0.63);">Victor Fernandes</li>
+						<li style="color: rgba(255, 255, 255, 0.61);">Todos los derechos reservados.</li></center>
+					</ul>
+				</div>
+			</footer>
+			
+			<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+			<script>
+				$(document).ready(function () {
+					$(".btn1").click(function () {
+						$("h1").slideUp(1000);
+						$("p").slideUp(1050);
+						$("h1").slideDown(1000);
+						$("p").slideDown(1050);
+
+					});
+				});
+			</script>
+				
+
+	</body>
+</html>
